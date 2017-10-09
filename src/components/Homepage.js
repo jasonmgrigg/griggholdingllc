35a3cbc90889e1f65a8b7d6b7d24e9b58c0490c1
Jason Grigg <boatdreamin@gmail.com>
2017-10-09 13:50:24 -0400

    react hover finally figured out

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,6 +5,99 @@ import Body from './Body.js'
 import Footer from './Footer.js';
 
 class Homepage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.showMore = this.showMore.bind(this, true);
+    this.handleMouseHoverGeneralCarpentry = this.handleMouseHoverGeneralCarpentry.bind(this);
+    this.handleMouseHoverPlumbing = this.handleMouseHoverPlumbing.bind(this);
+    this.handleMouseHoverLandscaping = this.handleMouseHoverLandscaping.bind(this);
+    this.handleMouseHoverAppleImacRepair = this.handleMouseHoverAppleImacRepair.bind(this);
+
+    this.state = {
+      isHoveringPlumbing: false,
+      isHoveringGeneralCarpentry: false,
+      isHoveringGeneralLandscaping: false,
+      isHoveringAppleImacRepair: false,
+      contributors: [
+        {
+          name: 'Brittany Arsi',
+          link: ''
+        },
+        {
+          name: 'Robert Garmhausen',
+          link: ''
+        },
+        {
+          name: 'Jason Grigg',
+          link: ''
+        },
+        {
+          name: 'Dylan Stump',
+          link: ''
+        }
+      ],
+      rowsToDisplay: 0,
+      expanded: false,
+      str: '',
+      browse: <div />,
+      status: this.props.status
+    };
+  }
+
+  handleStringChange(event) {
+    this.setState({ str: event.target.value });
+  }
+
+  showMore() {
+    this.state.rowsToDisplay === 0
+      ? this.setState({
+          rowsToDisplay: this.state.contributors.length,
+          expanded: true
+        })
+      : this.setState({ rowsToDisplay: 0, expanded: false });
+  }
+
+  handleMouseHoverPlumbing() {
+    this.setState(this.toggleHoverStatePlumbing);
+  }
+
+  handleMouseHoverGeneralCarpentry() {
+    this.setState(this.toggleHoverStateGeneralCarpentry);
+  }
+
+  handleMouseHoverLandscaping() {
+    this.setState(this.toggleHoverStateLandscaping);
+  }
+
+  handleMouseHoverAppleImacRepair() {
+    this.setState(this.toggleHoverStateAppleImacRepair);
+  }
+
+  toggleHoverStateGeneralCarpentry(state) {
+    return {
+      isHoveringGeneralCarpentry: !state.isHoveringGeneralCarpentry,
+    };
+  }
+
+  toggleHoverStatePlumbing(state) {
+    return {
+      isHoveringPlumbing: !state.isHoveringPlumbing,
+    };
+  }
+
+  toggleHoverStateLandscaping(state) {
+    return {
+      isHoveringLandscaping: !state.isHoveringLandscaping,
+    };
+  }
+
+  toggleHoverStateAppleImacRepair(state) {
+    return {
+      isHoveringAppleImacRepair: !state.isHoveringAppleImacRepair,
+    };
+  }
+
   render() {
     return (
       <div>
@@ -20,10 +113,43 @@ class Homepage extends Component {
             </h3>
             <ul className="servicesList">
               <li className="servicesItem">
-                General Carpentry
+                <div
+                  onMouseEnter={this.handleMouseHoverLandscaping}
+                  onMouseLeave={this.handleMouseHoverLandscaping}
+                  >
+                  Landscaping
+                </div>
+                {this.state.isHoveringLandscaping &&
+                  <div className="landscapingShowMore">
+                    We can work on any kind of wood or structural repair in your home, including your exterior porches and railings.
+                  </div>
+                }
               </li>
               <li className="servicesItem">
-                Plumbing
+                <div
+                  onMouseEnter={this.handleMouseHoverGeneralCarpentry}
+                  onMouseLeave={this.handleMouseHoverGeneralCarpentry}
+                  >
+                  General Carpentry
+                </div>
+                  {this.state.isHoveringGeneralCarpentry &&
+                  <div className="generalCarpentryShowMore">
+                    We can work on any kind of wood or structural repair in your home, including your exterior porches and railings.
+                  </div>
+                }
+              </li>
+              <li className="servicesItem">
+                <div
+                  onMouseEnter={this.handleMouseHoverPlumbing}
+                  onMouseLeave={this.handleMouseHoverPlumbing}
+                  >
+                  Plumbing
+                </div>
+                  {this.state.isHoveringPlumbing &&
+                  <div className="plumbingShowMore">
+                    We can work on any kind of wood or structural repair in your home, including your exterior porches and railings.
+                  </div>
+                }
               </li>
               <li className="servicesItem">
                 Roof Repair
@@ -61,18 +187,27 @@ class Homepage extends Component {
               <li className="servicesItem">
                 Screen Porch Repair
               </li>
-              <li className="servicesItem">
-                Landscaping
-              </li>
             </ul>
           </div>
           </div>
+
+        <div className="techServicesContainer">
           <div className="techServicesTitle">
             Tech Services
           </div>
           <ul className="techServicesList">
             <li className="servicesItem">
-              Apple Macintosh Repair
+              <div
+                onMouseEnter={this.handleMouseHoverAppleImacRepair}
+                onMouseLeave={this.handleMouseHoverAppleImacRepair}
+                >
+                Apple iMac Repair
+              </div>
+                {this.state.isHoveringAppleImacRepair &&
+                <div className="appleImacRepairShowMore">
+                  We can work on any kind of wood or structural repair in your home, including your exterior porches and railings.
+                </div>
+              }
             </li>
             <li className="servicesItem">
               Smartphone Repair
@@ -90,6 +225,8 @@ class Homepage extends Component {
               Web Development
             </li>
           </ul>
+        </div>
+
       <Footer />
       </div>
     );
