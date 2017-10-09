f9e08197722a685bfd00e28af6a355171c6c358b
Jason Grigg <boatdreamin@gmail.com>
2017-10-09 11:38:21 -0400

    homepage change

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../App.css';
+import '../styles/homepage.css';
 import Header from './Header.js'
 import Body from './Body.js'
 import Footer from './Footer.js';
@@ -9,7 +9,87 @@ class Homepage extends Component {
     return (
       <div>
       <Header />
-      <Body />
+        <div className="servicesContainer">
+        <div className="homeServicesTitle">Home Services</div>
+          <div className="servicesListContainer">
+            <h1 className="servicesTitle">
+              Put Your Feet Up And Let Us Do The Work!
+            </h1>
+            <h3 className="servicesDescription">
+              Take a look at the services we offer but do not hesitate to inquire about anything else, we a a full service company we just don't have room to put it all here!
+            </h3>
+            <ul className="servicesList">
+              <li className="servicesItem">
+                General Carpentry
+              </li>
+              <li className="servicesItem">
+                Plumbing
+              </li>
+              <li className="servicesItem">
+                Roof Repair
+              </li>
+              <li className="servicesItem">
+                Door Repair/Replacement
+              </li>
+              <li className="servicesItem">
+                Painting and Touchup
+              </li>
+              <li className="servicesItem">
+                Deck Maintenance
+              </li>
+              <li className="servicesItem">
+                Pressure Washing
+              </li>
+              <li className="servicesItem">
+                Gutter Cleaning
+              </li>
+              <li className="servicesItem">
+                Window Cleaning
+              </li>
+              <li className="servicesItem">
+                Picture Hanging
+              </li>
+              <li className="servicesItem">
+                Air Filter Replacement
+              </li>
+              <li className="servicesItem">
+                Smoke Detectors
+              </li>
+              <li className="servicesItem">
+                Car Washing
+              </li>
+              <li className="servicesItem">
+                Screen Porch Repair
+              </li>
+              <li className="servicesItem">
+                Landscaping
+              </li>
+            </ul>
+          </div>
+          </div>
+          <div className="techServicesTitle">
+            Tech Services
+          </div>
+          <ul className="techServicesList">
+            <li className="servicesItem">
+              Apple Macintosh Repair
+            </li>
+            <li className="servicesItem">
+              Smartphone Repair
+            </li>
+            <li className="servicesItem">
+              Home Networking
+            </li>
+            <li className="servicesItem">
+              Printer Maintenance
+            </li>
+            <li className="servicesItem">
+              Technical Training
+            </li>
+            <li className="servicesItem">
+              Web Development
+            </li>
+          </ul>
       <Footer />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -6,14 +6,12 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Homepage from './components/Homepage.js';
 import Contact from './components/Contact.js';
 import About from './components/About.js';
-import Services from './components/Services.js';
 
 ReactDOM.render(
   <BrowserRouter>
       <Switch>
         <Route path="/contact" component={Contact} />
         <Route path="/about" component={About} />
-        <Route path="/services" component={Services} />
         <Route path="/" component={Homepage} />
       </Switch>
   </BrowserRouter>,
