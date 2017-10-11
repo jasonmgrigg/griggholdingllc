82910459744a773d0c04f413578d7b6944f92690
Jason Grigg <boatdreamin@gmail.com>
2017-10-11 16:23:16 -0400

    complete redo

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,26 +13,16 @@ class Body extends Component {
 
   render() {
     return (
-      <div>
-        <div className="bodyContainer">
-          <div className="bodyTitle">
-
+      <div className="bodyContainer">
+        <div className="titleContainer">
+          <div className="titleText">
+            WELCOME TO GRIGG HOLDINGS, LLC
           </div>
-          <br />
-          <div className="bodyText">
-            Your one stop to repair nearly anything in your home!  Please click on our <a href="/services">Services</a> section for a list of our services.  Please note that this is just a small list, please let us know anything that you might need!
+          <div className="titleDescription">
+            Home and Technical Repair Services
           </div>
-          <br />
-          <div className="bodyText">
-            We are a company that specializes in repairs not only to things inside and outside your home, but for all of your technical needs as well.  We have years of experience repairing everything from minor plumbing items, interior/exterior touchup painting, picture hanging, furniture moving, minor landscaping, lawn mowing, car washing, and just about anything you can think of.  We are a company that offers assistance to do the small things that sometimes you just need an extra pair of hands to do.
-          </div>
-          <br />
-          <div className="bodyText">
-            Having trouble with your computer, smart phone, tablet, entertainment system, or anything else that is electronic?  We can help with that!  We have worked in the technical field for a long time, and can fix whatever is broken!
-          </div>
-          <br />
-          <div className="bodyText">
-            We bill in half hour increments, with very competitive rates and no service charge!  Being a local company we can come out as many times as you need us!  We like to tell our customers, make a list of the things you need, no matter how small, and let us take care of them one by one.  No job is too small, we are here to help any way we can!
+          <div className="phoneNumber">
+            (919) 932-0835
           </div>
         </div>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,19 +11,7 @@ class Header extends Component {
   render() {
     return (
       <div className="fullHeader">
-        <a className="headerTitle">Grigg Holdings, LLC</a>
-        <a className="headerTitleDescription">- The Full Service Home and Technical Service Company</a>
-        <div className="headerInfo">
-            <div className="headerSubtitle">
-                  919-932-0835
-                  <br />
-                  <a href="mailto: jason@jasonmgrigg.com">
-                  jason@griggholdingsllc.com
-                  </a>
-                </div>
-          </div>
-
-
+        <a className="headerTitle">GRIGG HOLDINGS, LLC</a>
         </div>
     );
   }
